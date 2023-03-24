dfd638005dd493abb14aaf1a144b5fb3e05e36c4
Samid <sabarrera21@ucatolica.edu.co>
2023-03-23 19:43:56 -0500

    Estrucutra Basica Hexagonal

---
+++ src/main/java/com/co/ucatolica/proyecto/infraestructure/config/LugarRuta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.co.ucatolica.proyecto.infraestructure.config;
+
+import jakarta.persistence.Entity;
+
+/**
+ *
+ * @author samid
+ */
+@Entity(name = "lugar_ruta")
+public class LugarRuta {
+    
+}
