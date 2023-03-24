d9c5224072386e45a2900858eb919a26aa6ca1a3
Samid <sabarrera21@ucatolica.edu.co>
2023-03-23 23:27:39 -0500

    Estrucutra Basica Hexagonal

--- src/main/java/com/co/ucatolica/proyecto/application/UsuarioInterface.java
+++ src/main/java/com/co/ucatolica/proyecto/application/UsuarioInterface.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.co.ucatolica.proyecto;
+package com.co.ucatolica.proyecto.application;
 
+import com.co.ucatolica.proyecto.infraestructure.config.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
---
+++ src/main/java/com/co/ucatolica/proyecto/domain/Temporal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.co.ucatolica.proyecto.domain;
+
+/**
+ *
+ * @author samid
+ */
+public class Temporal {
+    
+}
